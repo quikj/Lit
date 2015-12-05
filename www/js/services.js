08fd90d43b4932fab9cedc3b795f1c25d4ba62f4
bsudheesh <sudheesh.bhattarai@bison.howard.edu>
2015-12-05 00:45:48 -0500

    Assembling FBLogin, search option and Google maps

--- www/js/services.js
+++ www/js/services.js
@@ -28,9 +28,13 @@ angular.module('AppServices',[])
                   headers: authenticationHeaders
                 };
                 var dataObject = {
+                  "first": _params.first,
+                  "last": _params.last,
+                  //"phone": _params.phone,
                   "username": _params.username,
                   "password": _params.password,
-                  "email": _params.email
+                  "email": _params.email,
+                  //"birthdate": _params.birthdate
                 };
                 var dataObjectString = JSON.stringify(dataObject);
               return $http.post(baseURL + 'classes/_User', dataObjectString, settings)
@@ -88,4 +92,60 @@ angular.module('AppServices',[])
             }
 
         }
-    });
+    })
+  .service('FacebookAuth', function($http, $state, $q, $cordovaFacebook, ParseService){
+    var currentUser = {
+      "id": null
+    };
+
+    var login = function() {
+      return $cordovaFacebook.login(["public_profile", "email"])
+        .then(function (success) {
+          // save access_token
+          var accessToken = success.authResponse.accessToken;
+          var userID = success.authResponse.userID;
+          var expiresIn = success.authResponse.expiresIn;
+
+          console.log("Login Success" + JSON.stringify(success));
+
+          var expDate = new Date(
+            new Date().getTime() + expiresIn * 1000
+          ).toISOString();
+
+          var fbValues = "&fields=id,name,picture,email";
+          var fbPermission = ["public_profile", "email"];
+
+          $cordovaFacebook.api("me?access_token=" + accessToken + fbValues, fbPermission)
+            .then(function (_fbUserInfo) {
+              console.log("fbUserinfo", _fbUserInfo);
+              var UserData = {
+                "name": _fbUserInfo.name,
+                "email": _fbUserInfo.email,
+                "fb_id" : _fbUserInfo.id,
+                "photo": _fbUserInfo.picture.data.url,
+                "rsvps": [],
+                "events": [],
+              };
+              console.log("UserData", UserData);
+
+              ParseService.getUser(UserData.fb_id).then(function(result){
+                if (result.data.results.length != 0) {
+                  console.log("User exists in the database")
+                  return _fbUserInfo;
+                } else {
+
+                  return ParseService.addUser(UserData).then(function(success){
+                    console.log("User added", success);
+                    return success;
+                  })
+                }
+              })
+
+            });
+        });
+    };
+    return {
+      login: login,
+    }
+  });
+
